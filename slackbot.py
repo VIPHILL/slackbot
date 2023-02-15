4b8ff3b869ab1ac22678eba75f160c09debc1a8c
VIPHILL <125452964+VIPHILL@users.noreply.github.com>
2023-02-15 11:20:40 +0100

    Update slackbot.py
    Changed Webhook, due to private channel in Teams made public

--- slackbot.py
+++ slackbot.py
@@ -20,7 +20,7 @@ template.globals['now'] = datetime.now()
 msg = template.render(mensa = mensa, reimanns = reimanns)
 try:
     response = client.chat_postMessage(channel='#mittagspause', text=msg)
-    r=barker.request('POST','https://thide.webhook.office.com/webhookb2/1f4906f1-3c48-436b-905f-c27fa0d1d0f9@28bcace8-4ce7-4949-868f-170f67122379/IncomingWebhook/b9d2ae8eb70543db81d3398b14068f84/b7f56d59-b48b-48a3-a16f-3afae5b0976b', headers={'Content-Type': 'application/json'},
+    r=barker.request('POST','https://thide.webhook.office.com/webhookb2/1f4906f1-3c48-436b-905f-c27fa0d1d0f9@28bcace8-4ce7-4949-868f-170f67122379/IncomingWebhook/b9a816edd8bb44d69855f366614b0b49/b7f56d59-b48b-48a3-a16f-3afae5b0976b', headers={'Content-Type': 'application/json'},
 body=json.dumps({"text":msg}).encode('utf-8')
 
 except SlackApiError as e:
